cf2d7348afd3e391cef00a7744d4466c23d5cd30
Kenji Sagae <sagae@ucdavis.edu>
2024-04-15 16:52:53 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my_pip_package',
+    version='0.5',
+
+    url='https://github.com/sagae/fst',
+    author='Kenji Sagae',
+
+    py_modules=['fst'],
+)
